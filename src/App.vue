e8685d88912c90c36ee7c1fbed039e95110bb316
Angelo Pavilando <pavilandoangelo@gmail.com>
2020-10-11 21:46:26 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      app
+      v-model="drawer"
+      absolute
+      class="primary fill-height"
+      dark
+    >
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>John Leider</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="primary" dense dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>What movie to watch? Suprise Me!</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    drawer: true,
+    items: [
+      { title: 'Home', icon: 'mdi-view-dashboard' },
+      { title: 'About', icon: 'mdi-forum' },
+    ],
+  }),
+};
+</script>
