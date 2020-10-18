962f25af1da4a2c01bb3096c57477cd1dee723d6
Angelo Pavilando <pavilandoangelo@gmail.com>
2020-10-18 16:21:50 +0800

    🎨 Change Footer

--- src/App.vue
+++ src/App.vue
@@ -79,18 +79,16 @@
     <v-footer
       dark
       padless
-      fixed
     >
       <v-card
         class="flex"
         flat
         tile
       >
-        <v-card-title class="teal">
-          <strong class="subheading">Get connected with us on social networks!</strong>
 
-          <v-spacer></v-spacer>
-
+        <v-card-text class="py-2 white--text text-center">
+          {{ new Date().getFullYear() }} — <strong>Made with ❤️ by pavilandoangelo</strong>
+           <v-spacer></v-spacer>
           <v-btn
             v-for="icon in icons"
             :key="icon"
@@ -102,10 +100,6 @@
               {{ icon }}
             </v-icon>
           </v-btn>
-        </v-card-title>
-
-        <v-card-text class="py-2 white--text text-center">
-          {{ new Date().getFullYear() }} — <strong>Made with ❤️ by pavilandoangelo</strong>
         </v-card-text>
       </v-card>
     </v-footer>
